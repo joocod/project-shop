ae845ba4a330d6933240ffde8d615372bb738bfe
joocod <20634hdj@gmail.com>
2024-01-05 18:44:23 +0900

    commit 01/05

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import GlobalStyle from './style/GlobalStyle';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
+      <QueryClientProvider client={queryClient}>
+         <GlobalStyle/>
+      </QueryClientProvider>
     </>
   );
 }
